0141eedb06f37a3c9670b6b67b12f66761ac251c
MiroslavSlouka <miroslav.slouka@techfides.cz>
2023-08-28 21:29:17 +0200

    rewriting server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,34 @@
+require('dotenv').config();
+
 import express from 'express';
 import mysql from 'mysql';
-// import * as player from '../src/api/requests/api-football/competitions.js';
+// import * as player from './requests/api-football/competitions';
 
 const app = express();
-const database = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.MYSQL_PASS,
-  database: 'continents',
+// TODO: endpoints will be initialized here
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const database = mysql.createConnection({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
+
+const PORT = process.env.PORT;
 database.connect((err) => {
   if (err) {
-    console.error(`Error connecting to server: ${err}`);
-  } else {
-    console.log('Connected to database');
-    app.listen(5000, () => {
-      console.log('Server Connected');
-    });
+    console.error(`Error connecting to database: ${err.code}`);
+    process.exit(1);
   }
+  console.log('Connected to database');
+
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+  });
 });
