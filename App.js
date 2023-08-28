035c7cd1ed0bcbb8ec3cb81c4cda56606c8f6283
TKPending <thatguyt70@outlook.com>
2023-08-28 19:15:35 +0100

    Update

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 // Global Imports
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 // Private Imports
-// THIS RUNS THE WHOLE APP
 import Footsie from './src/pages/foundation/footsie';
 
 export default function App() {
 
   return (
     <View style={[application.container, {justifyContent: 'center', alignItems: 'center'}]}>
-      <Text style={{fontSize: 20}}>Testing Ground</Text>
-      <Text style={{fontSize: 20}}>No API Calls Made</Text>
+      <Footsie />
     </View>
   );
 }
