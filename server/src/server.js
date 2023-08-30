03094c1d65159d8160fe03d5557668cff445eeb3
TKPending <thatguyt70@outlook.com>
2023-08-30 01:43:10 +0100

    Temporary updates

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,39 @@ require('dotenv').config();
 
 import express from 'express';
 import mysql from 'mysql';
-// import * as player from './requests/api-football/competitions';
+import * as player from './requests/api-football/competitions';
 
 const app = express();
+
 // TODO: endpoints will be initialized here
+const test = async () => {
+  try {
+    const response = await player.getCountryNameAndFlags();
+    const countries = response.countries;
+
+    // Iterate through each key-value pair in the object
+    for (const countryName in countries) {
+      if (countries.hasOwnProperty(countryName)) {
+        const countryInfo = countries[countryName];
+        const { countryURL, countryTag } = countryInfo;
+
+        // Insert the data into the database
+        const query = `INSERT INTO countries (countryName, countryURL, countryTag) VALUES (?, ?, ?)`;
+        database.query(query, [countryName, countryURL, countryTag], (err, result) => {
+          if (err) {
+            console.error(`Error inserting data into database: ${err}`);
+          } else {
+            console.log(`Inserted data into database: ${result.insertId}`);
+          }
+        });
+      }
+    }
+
+    return countries;
+  } catch (err) {
+    console.error("Error with API call");
+  }
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
